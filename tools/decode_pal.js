3e1350c88a182804e5cfca4319cae43ebe8a9001
Dan Adeholt <dan@binarypeak.se>
2021-03-17 19:50:04 +0100

    Added tool to convert .pal files to array constant format

---
+++ tools/decode_pal.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+let path = '../../fceux/output/palettes/FCEUX.pal';
+
+const hex = num => num.toString(16).toUpperCase().padStart(2, '0');
+
+console.log(path);
+
+const data = fs.readFileSync(path);
+
+for (let i = 0, addr = 0; addr < data.length; i++) {
+  const r = data[addr++];
+  const g = data[addr++];
+  const b = data[addr++];
+  console.log('0xFF' + hex(r) + hex(g) + hex(b) + ', //' + hex(i++));
+}
+
+
