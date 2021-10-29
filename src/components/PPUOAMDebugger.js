cd288922ab5baf38e04e9ba18e9564a1b1361098
Dan Adeholt <dan@binarypeak.se>
2021-10-29 20:17:51 +0200

    PPUOAMDebugger: Improve attribute prints

--- src/components/PPUOAMDebugger.js
+++ src/components/PPUOAMDebugger.js
@@ -1,5 +1,12 @@
 import React, { useMemo } from 'react';
 import _ from 'lodash';
+import {
+  SPRITE_ATTRIB_FLIP_HORIZONTAL,
+  SPRITE_ATTRIB_FLIP_VERTICAL,
+  SPRITE_ATTRIB_PRIORITY,
+  SPRITE_ATTRIBS_PALETTE
+} from '../emulator/ppu';
+import { hex } from '../emulator/stateLogging';
 
 const PPUOAMDebugger = ({ refresh, emulator}) => {
   const lines = useMemo(() => {
@@ -14,9 +21,12 @@ const PPUOAMDebugger = ({ refresh, emulator}) => {
       ret.push({
         y: emulator.ppu.oamMemory[i],
         x: emulator.ppu.oamMemory[i + 3],
-        tile: emulator.ppu.oamMemory[i + 1],
-        attribs: emulator.ppu.oamMemory[i + 2]
-        });
+        tile: hex(emulator.ppu.oamMemory[i + 1]),
+        flipHorizontal: (emulator.ppu.oamMemory[i + 2] & SPRITE_ATTRIB_FLIP_HORIZONTAL) >> 6,
+        flipVertical: (emulator.ppu.oamMemory[i + 2] & SPRITE_ATTRIB_FLIP_VERTICAL) >> 7,
+        priority: (emulator.ppu.oamMemory[i + 2] & SPRITE_ATTRIB_PRIORITY) >> 5,
+        palette: (emulator.ppu.oamMemory[i + 2] & SPRITE_ATTRIBS_PALETTE)
+      });
     }
 
     return ret;
@@ -29,7 +39,7 @@ const PPUOAMDebugger = ({ refresh, emulator}) => {
 
         { lines.map((line, idx) => (
           <div>
-            { idx + ' - ' + line.x + ',' + line.y + ' - ' + line.tile + ' - ' + line.attribs}
+            { idx + ' - ' + line.x + ',' + line.y + ' - ' + line.tile + ' - FlipH  ' + line.flipHorizontal + ', FlipV' + line.flipVertical + ', Prio: ' + line.priority + ', Palette: ' + line.palette }
           </div>
         ))
         }
