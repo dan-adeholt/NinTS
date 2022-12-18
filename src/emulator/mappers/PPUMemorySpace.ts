bf53eb01dde89544410a1cdd5a3e8875dc0163a9
Dan Adeholt <dan@binarypeak.se>
2022-12-18 16:28:25 +0100

    PPUMemorySpace: Mirror various areas, disable writes if not supported

--- src/emulator/mappers/PPUMemorySpace.ts
+++ src/emulator/mappers/PPUMemorySpace.ts
@@ -8,9 +8,12 @@ class PPUMemorySpace {
   namespaceRam = new Uint8Array(4096); // Make it 4KB to allow for 4 screen mirroring
   mirroringMode = MirroringMode.Vertical;
   chrSource: Uint8Array
+  chrRamSize = 0
 
   constructor(rom: Rom) {
     this.chrRam = new Uint8Array(rom.settings.chrRamSize);
+    this.chrRamSize = rom.settings.chrRamSize;
+
     if (rom.settings.chrRamSize > 0) {
       this.chrSource = this.chrRam;
     } else {
@@ -35,6 +38,10 @@ class PPUMemorySpace {
   }
 
   write(address: number, value: number) {
+    if (address < 0x2000 && this.chrRamSize === 0) {
+      return;
+    }
+
     this.memory.write(address, value);
   }
 
@@ -46,29 +53,39 @@ class PPUMemorySpace {
     this.mirroringMode = mirroringMode;
     switch(mirroringMode) {
       case MirroringMode.SingleScreenUpper:
-        this.memory.map(this.namespaceRam, 0x2000, 0x0000, 0x0400);
-        this.memory.map(this.namespaceRam, 0x2400, 0x0000, 0x0400);
-        this.memory.map(this.namespaceRam, 0x2800, 0x0000, 0x0400);
-        this.memory.map(this.namespaceRam, 0x2C00, 0x0000, 0x0400);
+        for (let mirrorOffset = 0; mirrorOffset <= 0x1000; mirrorOffset += 0x1000) {
+          this.memory.map(this.namespaceRam, 0x2000 + mirrorOffset, 0x0000, 0x0400);
+          this.memory.map(this.namespaceRam, 0x2400 + mirrorOffset, 0x0000, 0x0400);
+          this.memory.map(this.namespaceRam, 0x2800 + mirrorOffset, 0x0000, 0x0400);
+          this.memory.map(this.namespaceRam, 0x2C00 + mirrorOffset, 0x0000, 0x0400);
+        }
         break;
       case MirroringMode.SingleScreenLower:
-        this.memory.map(this.namespaceRam, 0x2000, 0x0400, 0x0800);
-        this.memory.map(this.namespaceRam, 0x2400, 0x0400, 0x0800);
-        this.memory.map(this.namespaceRam, 0x2800, 0x0400, 0x0800);
-        this.memory.map(this.namespaceRam, 0x2C00, 0x0400, 0x0800);
+        for (let mirrorOffset = 0; mirrorOffset <= 0x1000; mirrorOffset += 0x1000) {
+          this.memory.map(this.namespaceRam, 0x2000 + mirrorOffset, 0x0400, 0x0800);
+          this.memory.map(this.namespaceRam, 0x2400 + mirrorOffset, 0x0400, 0x0800);
+          this.memory.map(this.namespaceRam, 0x2800 + mirrorOffset, 0x0400, 0x0800);
+          this.memory.map(this.namespaceRam, 0x2C00 + mirrorOffset, 0x0400, 0x0800);
+        }
         break;
       case MirroringMode.Horizontal:
-        this.memory.map(this.namespaceRam, 0x2000, 0x0000, 0x0400);
-        this.memory.map(this.namespaceRam, 0x2400, 0x0000, 0x0400);
-        this.memory.map(this.namespaceRam, 0x2800, 0x0400, 0x0800);
-        this.memory.map(this.namespaceRam, 0x2C00, 0x0400, 0x0800);
+        for (let mirrorOffset = 0; mirrorOffset <= 0x1000; mirrorOffset += 0x1000) {
+          this.memory.map(this.namespaceRam, 0x2000 + mirrorOffset, 0x0000, 0x0400);
+          this.memory.map(this.namespaceRam, 0x2400 + mirrorOffset, 0x0000, 0x0400);
+          this.memory.map(this.namespaceRam, 0x2800 + mirrorOffset, 0x0400, 0x0800);
+          this.memory.map(this.namespaceRam, 0x2C00 + mirrorOffset, 0x0400, 0x0800);
+        }
         break;
       case MirroringMode.Vertical:
-        this.memory.map(this.namespaceRam, 0x2000, 0x0000, 0x0800);
-        this.memory.map(this.namespaceRam, 0x2800, 0x0000, 0x0800);
+        for (let mirrorOffset = 0; mirrorOffset <= 0x1000; mirrorOffset += 0x1000) {
+          this.memory.map(this.namespaceRam, 0x2000 + mirrorOffset, 0x0000, 0x0800);
+          this.memory.map(this.namespaceRam, 0x2800 + mirrorOffset, 0x0000, 0x0800);
+        }
         break;
       case MirroringMode.FourScreen:
-        this.memory.map(this.namespaceRam, 0x2000, 0x0000, 0x1000);
+        for (let mirrorOffset = 0; mirrorOffset <= 0x1000; mirrorOffset += 0x1000) {
+          this.memory.map(this.namespaceRam, 0x2000 + mirrorOffset, 0x0000, 0x1000);
+        }
         break;
       default:
         console.error('Unrecognized mirroring mode', mirroringMode);
